c3ed665918ec3fa5d57a55fcb60ba28411ce9980
Ralph Kube <ralph.kube@uit.not>
2023-12-17 13:26:13 -0800

    Completed day 4, part 2

--- 04/aoc_04.go
+++ 04/aoc_04.go
@@ -47,17 +47,56 @@ func parse_seq(seq string) []int {
 //  3. Split each split from step 2 by " ", and parse each sub string as an int to get the
 //     integer values of the splits.
 func parse_card(card string) ([]int, []int) {
-	fmt.Printf("%s\n", card)
 	//  Remove first 9 characters and split
 	// card_sp := card[9:len(card)]
 	// fmt.Printf("--- After pruning: %s\n", card_sp)
 	splits_1 := strings.Split(card[9:], "|")
-	fmt.Printf("--- Split 1 (winning numbers): %s\n", splits_1[0])
-	fmt.Printf("--- Split 2 (drawn numbers): %s\n", splits_1[1])
+	// fmt.Printf("--- Split 1 (winning numbers): %s\n", splits_1[0])
+	// fmt.Printf("--- Split 2 (drawn numbers): %s\n", splits_1[1])
 	winning_numbers := parse_seq(splits_1[0])
 	drawn_numbers := parse_seq(splits_1[1])
 
 	return winning_numbers, drawn_numbers
+}
+
+// Calculate integer powers
+func IntPow(n, m int) int {
+	if m == 0 {
+		return 1
+	}
+	result := n
+	for i := 2; i <= m; i++ {
+		result *= n
+	}
+	return result
+}
+
+// Scores the card.
+// In the above example, card 1 has five winning numbers (41, 48, 83, 86, and 17) and
+// eight numbers you have (83, 86, 6, 31, 17, 9, 48, and 53). Of the numbers you have,
+// four of them (48, 83, 17, and 86) are winning numbers! That means card 1 is worth 8
+// points (1 for the first match, then doubled three times for each of the three matches
+// after the first).
+func score_card(winning_numbers []int, drawn_numbers []int) (int, int) {
+	// For each winning number, search if it matches a drawn number.
+	// Then count the matches
+	num_matches := 0
+	// fmt.Printf("len(w) = %d, len(d) = %d\n", len(winning_numbers), len(drawn_numbers))
+	for _, w := range winning_numbers {
+		for _, d := range drawn_numbers {
+			// fmt.Printf("[%d - %d] and  [%d - %d]\n", ix_w, w, ix_d, d)
+			if d == w {
+				// fmt.Printf("==================== Match: [%d - %d] and [%d - %d]\n", ix_w, w, ix_d, d)
+				num_matches += 1
+			}
+		}
+		// fmt.Printf("\n")
+	}
+	if num_matches > 0 {
+		return num_matches, IntPow(2, num_matches-1)
+	} else {
+		return num_matches, 0
+	}
 }
 
 func main() {
@@ -70,22 +109,54 @@ func main() {
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 
+	total_score := 0 // This accumulates the score reported for part 1.
+	ix_card := 0
+
+	card_counts := make([]int, 192, 200) // For part 2: Counts the copies of cards that are won
+	card_scores := make([]int, 192, 200)
+	// Initially, we have 1 copy of each card
+	for ix_c := range card_counts {
+		card_counts[ix_c] = 1
+	}
+
 	for scanner.Scan() {
+		fmt.Printf("%d\n", ix_card)
 		current_card := scanner.Text()
 		// Parse the current card
 		winning_numbers, drawn_numbers := parse_card(current_card)
 
-		fmt.Printf("winning numbers (as integers)\n\t")
-		for ix_w, _ := range winning_numbers {
-			fmt.Printf("%d ", winning_numbers[ix_w])
-		}
-		fmt.Printf("\n")
+		// Now score the card
+		num_matches, this_score := score_card(winning_numbers, drawn_numbers)
+		card_scores[ix_card] = num_matches
+		total_score += this_score
 
-		fmt.Printf("Drawn numbers (as integers)\n\t")
-		for ix_d, _ := range drawn_numbers {
-			fmt.Printf("%d ", drawn_numbers[ix_d])
+		// fmt.Printf("card %d: %d matches\n", ix_card+1, num_matches)
+
+		// For part two, we add one copy to each of the next s cards, where s is the number of winning numbers
+		for s := 1; s <= num_matches; s++ {
+			if (ix_card + s) < len(card_counts) {
+				card_counts[ix_card+s] += card_counts[ix_card]
+			}
 		}
-		fmt.Printf("\n")
+
+		ix_card += 1
+		if ix_card < 5 {
+			for ix_s := 0; ix_s < len(card_counts); ix_s++ {
+				fmt.Printf("[%d] : %03d\n", ix_s, card_counts[ix_s])
+			}
+		}
+	}
+	fmt.Printf("Total score from all cards: %d\n", total_score)
+	// Correct answer: 21213
+
+	for ix_s := 0; ix_s < len(card_counts); ix_s++ {
+		fmt.Printf("[%d] : %03d\n", ix_s, card_scores[ix_s])
+	}
+	sum_cards := 0
+	for _, c := range card_counts {
+		sum_cards += c
 	}
 
+	fmt.Printf("Total number of cards won: %d\n", sum_cards)
+	// Correct answer: 8549735
 }
