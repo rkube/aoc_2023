1368e7214eb18c01f4771f086434653033880702
Ralph Kube <ralph.kube@uit.not>
2023-12-19 11:44:42 -0800

    Solved Day 6, part 1

---
+++ 06/aoc_06.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Advent of code 2023 - Part 06\n")
+	durations := []int{40, 70, 98, 79}        // Durations of the race
+	distances := []int{215, 1051, 2147, 1005} // Distance record
+
+	margin := 1
+	for ix := 0; ix < 4; ix++ {
+		// Calculate distance travelled for every possible duration of charging time
+		this_dur := durations[ix]
+		record_distance := distances[ix]
+		ways_to_beat_record := 0
+		for v := 0; v < this_dur; v++ {
+			remaining_time := this_dur - v
+			if v*remaining_time > record_distance {
+				// fmt.Printf("%d * %d = %d > %d\n", v, remaining_time, v*remaining_time, record_distance)
+				ways_to_beat_record += 1
+			}
+		}
+		margin *= ways_to_beat_record
+	}
+	// Correct answer: 1084752
+	fmt.Printf("Part 1: margin = %d\n", margin)
+
+	// Part 2
+}
---
+++ 06/go.mod
@@ -0,0 +1,3 @@
+module aoc_06
+
+go 1.21.5
