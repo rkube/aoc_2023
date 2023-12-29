a9d02a265b7782316c6a2238ab5d25b17a79a466
Ralph Kube <ralph.kube@uit.not>
2023-12-28 20:36:45 -0800

    Clarifying comments

--- 10/aoc_10.go
+++ 10/aoc_10.go
@@ -155,6 +155,15 @@ func setup_maze(filename string, len int) ([]string, position) {
 // Starting at a line, all points not connected to the curve are taken to be outside
 // After crossing the line the first time, points are taken to be inside
 // Crossing the line a second time, points are outside again etc.
+//
+// Vertical crossings are either
+//  1. |  - Crossing a vertical pipe
+//  2. L------7  - Crossing a L, closed by a 7, and connected by an arbitrary number of vertical pipes
+//  3. F------J  - Crossing a F, closed by a J, and connected by an arbitrary number of vertical pipes
+//
+// Vertical crossings are not
+//  1. L-----J - This is a U-turn, the ray propagates tangential to the pipe
+//  1. F-----7 - This is a U-turn, the ray propagates tangential to the pipe
 func ray_casting(trace_line []byte) ([]byte, int, int) {
 	num_crossings := 0 // Number of times we have crossed the line
 
@@ -165,9 +174,11 @@ func ray_casting(trace_line []byte) ([]byte, int, int) {
 	hold_F := false // or a u-turn.
 
 	// Iterate over the line and either
-	// count a line crossing, which happens when we come across a line element that is in '|LJ7F', but not '-'
-	// count an inside/outside point,
-	// or jump over a pipe connection
+	// a) count a line crossing,
+	// b) Open a hold for a L or F
+	// c) Close a hold from an L or F when coming across an J or 7. Distinguish  between crossing and U-turn
+	// d) count an inside/outside point (when there is a '.')
+	// e) Continue on horizontal pipe '-'
 	for ix, _ := range trace_line {
 		if trace_line[ix] == '.' {
 			if num_crossings%2 == 0 {
@@ -191,7 +202,7 @@ func ray_casting(trace_line []byte) ([]byte, int, int) {
 				continue
 			} else {
 				hold_L = true
-				fmt.Printf("[%d]%c - setting hold_L=%v\n", ix, trace_line[ix], hold_L)
+				// fmt.Printf("[%d]%c - setting hold_L=%v\n", ix, trace_line[ix], hold_L)
 				continue
 			}
 		} else if trace_line[ix] == 'F' {
@@ -202,7 +213,7 @@ func ray_casting(trace_line []byte) ([]byte, int, int) {
 				continue
 			} else {
 				hold_F = true
-				fmt.Printf("[%d]%c - setting hold_F=%v\n", ix, trace_line[ix], hold_F)
+				// fmt.Printf("[%d]%c - setting hold_F=%v\n", ix, trace_line[ix], hold_F)
 				continue
 			}
 		} else if trace_line[ix] == '7' {
@@ -211,12 +222,12 @@ func ray_casting(trace_line []byte) ([]byte, int, int) {
 				// If an L-hold is active, count a crossing
 				hold_L = false
 				num_crossings += 1
-				fmt.Printf("[%d]%c - setting hold_L=%v - counting crossing\n", ix, trace_line[ix], hold_L)
+				// fmt.Printf("[%d]%c - setting hold_L=%v - counting crossing\n", ix, trace_line[ix], hold_L)
 				continue
 			} else if hold_L == false && hold_F == true {
 				// If an F-hold is active, this is a U-turn
 				hold_F = false
-				fmt.Printf("[%d]%c - setting hold_F=%v - U-turn\n", ix, trace_line[ix], hold_F)
+				// fmt.Printf("[%d]%c - setting hold_F=%v - U-turn\n", ix, trace_line[ix], hold_F)
 				continue
 			} else {
 				fmt.Printf("This should not happen: trace_line[%d] = %c and hold_L = %v, hold_F = %v\n", ix, trace_line[ix], hold_L, hold_F)
@@ -226,12 +237,12 @@ func ray_casting(trace_line []byte) ([]byte, int, int) {
 			if hold_L == false && hold_F == true {
 				// If a F-hold is active, count a crossing
 				hold_F = false
-				fmt.Printf("[%d]%c - setting hold_F=%v - counting crossing\n", ix, trace_line[ix], hold_F)
+				// fmt.Printf("[%d]%c - setting hold_F=%v - counting crossing\n", ix, trace_line[ix], hold_F)
 				num_crossings += 1
 				continue
 			} else if hold_L == true && hold_F == false {
 				// If a L-hold is active, this is a U-turn
-				fmt.Printf("[%d]%c - setting hold_L=%v - U-turn\n", ix, trace_line[ix], hold_L)
+				// fmt.Printf("[%d]%c - setting hold_L=%v - U-turn\n", ix, trace_line[ix], hold_L)
 				hold_L = false
 				continue
 			} else {
@@ -244,64 +255,7 @@ func ray_casting(trace_line []byte) ([]byte, int, int) {
 			fmt.Printf("Unexxpected token in trace_line:%d  - %c\n", ix, trace_line[ix])
 		}
 	}
-	// else if  trace_line[ix] == 'L' || trace_line[ix] == 'F' {
-	// 	if hold == false {
-	// 		fmt.Printf("[%d] %c: setting hold=true\n", ix, trace_line[ix])
-	// 		IO_line[ix] = trace_line[ix]
-	// 		hold = true
-	// 		num_crossings += 1
-	// 		continue
-	// 	} else {
-	// 		fmt.Printf("This should not happen: trace_line[ix] == %c and hold=%v", trace_line[ix], hold)
-	// 	}
-	// } else if trace_line[ix] == 'S' || trace_line[ix] == '7' || trace_line[ix] == 'J' {
-	// 	if hold {
-	// 		fmt.Printf("[%d] %c: setting hold=false\n", ix, trace_line[ix])
-	// 		IO_line[ix] = trace_line[ix]
-	// 		hold = false
-	// 		num_crossings += 1
-	// 		continue
-	// 	} else {
-	// 		fmt.Printf("This should not happen: trace_line[ix] == %c and hold=%v", trace_line[ix], hold)
-	// 	}
-	// }
-	// else if trace_line[ix] == '-' {
-	// 	IO_line[ix] = trace_line[ix]
-	// 	continue
-	// }
-	// if trace_line[ix] == '+' {
-	// 	// If we come across the line we have to test whether we cross it, or move along the line
-	// 	IO_line[ix] = '+'
-	// 	if maze_line[ix] == '|' {
-	// 		num_crossings += 1
-	// 		continue
-	// 	} else if maze_line[ix] == '-' {
-	// 		// Move along the line
-	// 		continue
-	// 	}
-	// 	if hold == false && (maze_line[ix] == 'L' || maze_line[ix] == 'F') {
-	// 		hold = true
-	// 		continue
-	// 	} else if hold && (maze_line[ix] == '7' || maze_line[ix] == 'J') {
-	// 		hold = false
-	// 		num_crossings += 1
-	// 		continue
-	// 	}
-	// } else if trace_line[ix] == '.' && maze_line[ix] == '.' {
-	// 	// Count '.' character as either inside or outside
-	// 	if num_crossings%2 == 0 {
-	// 		IO_line[ix] = 'O'
-	// 		O_ctr += 1
-	// 	} else if num_crossings%2 == 1 {
-	// 		IO_line[ix] = 'I'
-	// 		I_ctr += 1
-	// 	}
-	// 	continue
-	// } else if trace_line[ix] == '.' && maze_line[ix] != '.' {
-	// 	continue
-	// }
-
-	fmt.Printf("%s\n", IO_line)
+	// fmt.Printf("%s\n", IO_line)
 	// fmt.Printf("I: %d, O: %d\n", I_ctr, O_ctr)
 
 	return IO_line, I_ctr, O_ctr
@@ -309,30 +263,48 @@ func ray_casting(trace_line []byte) ([]byte, int, int) {
 
 func main() {
 	fmt.Printf("Advent of code 2023 - Part 10\n")
-	maze, curr_pos := setup_maze("input_10", 140)
+	len := 140
+	maze, curr_pos := setup_maze("input_10", len)
+	trace_arr := make([]byte, len*len)
+
+	// Initialize the tracing array with all '.' for readability
+	for ix, _ := range trace_arr {
+		trace_arr[ix] = '.'
+	}
+
 	prev_pos := curr_pos
 	next_pos := curr_pos
 	fmt.Printf("Start node: [%02d, %02d]: %c (%d)\n", curr_pos.x, curr_pos.y, curr_pos.pipe, curr_pos.distance)
+	trace_arr[curr_pos.y*len+curr_pos.x] = '|' // maze[curr_pos.y][curr_pos.x]
 
 	for {
-		fmt.Printf("----------- Step ----------\n")
+		// fmt.Printf("----------- Step ----------\n")
 		next_pos, err := maze_step(curr_pos, prev_pos, maze)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("New node: [%02d, %02d]: %c (%d)\n", next_pos.x, next_pos.y, next_pos.pipe, next_pos.distance)
+		// fmt.Printf("New node: [%02d, %02d]: %c (%d)\n", next_pos.x, next_pos.y, next_pos.pipe, next_pos.distance)
 		// Step through the maze until we are back at the start
 		if next_pos.pipe == 'S' {
 			break
 		}
 		prev_pos = curr_pos
 		curr_pos = next_pos
+		trace_arr[curr_pos.y*len+curr_pos.x] = maze[curr_pos.y][curr_pos.x]
 	}
 
 	// The node farthest away from the start is found by taking the length
 	// of the entire round trip divided by 2.
 	// Solution: 13434/2 = 6717
 	fmt.Printf("Total length of the path: %d. Answer: %d\n", next_pos.distance, next_pos.distance/2)
+	I_total := 0
+	O_total := 0
+	for iy := 0; iy < len; iy++ {
+		_, I_line, O_line := ray_casting(trace_arr[iy*len : (iy+1)*len-1])
+		I_total += I_line
+		O_total += O_line
+	}
 
-	// fmt.Printf("[%d, %d]: %c %d\n", start_pos.x, start_pos.y, start_pos.pipe, start_pos.distance)
+	fmt.Printf("Internal area: %d\n", I_total)
+
 }
