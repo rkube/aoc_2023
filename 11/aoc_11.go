cfe5ffe3fa08a6ce5ea34fd11eb86c9e1b03039e
Ralph Kube <ralph.kube@uit.not>
2023-12-29 21:04:21 -0800

    Correct solution using sparse representation

--- 11/aoc_11.go
+++ 11/aoc_11.go
@@ -59,8 +59,8 @@ func find_empty_cols(chart []byte, nrow int, ncol int) []int {
 }
 
 // Duplicate empty rows
-func duplicate_row(chart []byte, nrow int, ncol int, duplicate_rows []int) []byte {
-	new_nrow := nrow + len(duplicate_rows)
+func duplicate_row(chart []byte, nrow int, ncol int, duplicate_rows []int, n_dup int) []byte {
+	new_nrow := nrow + n_dup*len(duplicate_rows)
 	new_chart := make([]byte, new_nrow*ncol)
 
 	ix_new_row := 0 // Used to index the new array
@@ -73,12 +73,17 @@ func duplicate_row(chart []byte, nrow int, ncol int, duplicate_rows []int) []byt
 		for _, v := range duplicate_rows {
 			// We have to check if the current original row needs to be duplicated.
 			if ix_row == v {
-				// fmt.Printf("Duplicating: ix_row = %d -> ix_new_row = %d\n", ix_row, ix_new_row)
-				ix_new_row += 1
-				for ix_col := 0; ix_col < ncol; ix_col++ {
-					new_chart[ix_new_row*ncol+ix_col] = chart[ix_row*ncol+ix_col]
+				// Insert n_dup new rows at this position. Start iteration variable
+				// at the next line, ix_new_row+1.
+				// for ; ix_new_row < ix_new_row+n_dup; ix_new_row++ {
+				for ix_this := ix_new_row + 1; ix_this < ix_new_row+n_dup+1; ix_this++ {
+					// fmt.Printf("Duplicating: (n_dup = %d) ix_row = %d -> ix_new_row = %d\n", n_dup, ix_row, ix_this)
+					for ix_col := 0; ix_col < ncol; ix_col++ {
+						new_chart[ix_this*ncol+ix_col] = chart[ix_row*ncol+ix_col]
+					}
+					// fmt.Printf("-done\n")
 				}
-
+				ix_new_row += n_dup
 			}
 		}
 		ix_new_row += 1
@@ -86,8 +91,8 @@ func duplicate_row(chart []byte, nrow int, ncol int, duplicate_rows []int) []byt
 	return new_chart
 }
 
-func duplicate_col(chart []byte, nrow int, ncol int, duplicate_cols []int) []byte {
-	new_ncol := ncol + len(duplicate_cols)
+func duplicate_col(chart []byte, nrow int, ncol int, duplicate_cols []int, n_dup int) []byte {
+	new_ncol := ncol + n_dup*len(duplicate_cols)
 	new_chart := make([]byte, nrow*new_ncol)
 
 	ix_new_col := 0
@@ -98,8 +103,10 @@ func duplicate_col(chart []byte, nrow int, ncol int, duplicate_cols []int) []byt
 			// Check if current column needs to be duplicated
 			for _, v := range duplicate_cols {
 				if ix_col == v {
-					ix_new_col += 1
-					new_chart[ix_row*new_ncol+ix_new_col] = chart[ix_row*ncol+ix_col]
+					for ix_this := ix_new_col + 1; ix_this < ix_new_col+n_dup+1; ix_this++ {
+						new_chart[ix_row*new_ncol+ix_this] = chart[ix_row*ncol+ix_col]
+					}
+					ix_new_col += n_dup
 				}
 			}
 			ix_new_col += 1
@@ -144,6 +151,31 @@ func galaxy_pos(chart []byte, nrow int, ncol int) []coords_t {
 	return galaxy_coords
 }
 
+// Count how many values in the array skips are smaller than coord
+func smaller_in_array(coord int, skips []int) int {
+	num_smaller := 0
+	for _, v := range skips {
+		if v < coord {
+			num_smaller += 1
+		}
+	}
+	return num_smaller
+}
+
+// Calculate the shifted position
+// Shift the row/col coordinate of a galaxy by n_dup
+// for each row/col that precedes the galaxy position
+func shift_position(galaxy_coords coords_t, duplicate_rows []int, duplicate_cols []int, n_dup int) coords_t {
+	// fmt.Printf("Original coordinate: [%03d,%03d]\n", galaxy_coords.row, galaxy_coords.col)
+	num_shift_rows := smaller_in_array(galaxy_coords.row, duplicate_rows)
+	num_shift_cols := smaller_in_array(galaxy_coords.col, duplicate_cols)
+	// fmt.Printf("\tShift rows: %d\n", num_shift_rows*n_dup)
+	// fmt.Printf("\tShift cols: %d\n", num_shift_cols*n_dup)
+
+	return coords_t{id: galaxy_coords.id, row: galaxy_coords.row + n_dup*num_shift_rows,
+		col: galaxy_coords.col + n_dup*num_shift_cols}
+}
+
 func mahattan_dist(g1 coords_t, g2 coords_t) int {
 	return Abs(g1.col-g2.col) + Abs(g1.row-g2.row)
 }
@@ -151,6 +183,51 @@ func mahattan_dist(g1 coords_t, g2 coords_t) int {
 func main() {
 	fmt.Printf("Advent of Code 2023 - Day 11\n")
 
-	chart := load_map("input_11", 141, 141)
-	fmt.Printf("%s\n", chart[0:141])
+	nrow, ncol := 140, 140
+	chart := load_map("input_11", nrow, ncol)
+	empty_rows := find_empty_rows(chart, nrow, ncol)
+	empty_cols := find_empty_cols(chart, nrow, ncol)
+	n_dup := 1
+	// new_nrow := nrow + n_dup*len(empty_rows)
+	// new_ncol := ncol + n_dup*len(empty_cols)
+
+	// chart_exp_rows := duplicate_row(chart, nrow, ncol, empty_rows, n_dup)
+	// chart_exp := duplicate_col(chart_exp_rows, new_nrow, ncol, empty_cols, n_dup)
+
+	galaxy_coords := galaxy_pos(chart, nrow, ncol)
+	galaxy_coords_shift := []coords_t{}
+	fmt.Printf("%d galaxies\n", len(galaxy_coords))
+	for _, g := range galaxy_coords {
+		galaxy_coords_shift = append(galaxy_coords_shift, shift_position(g, empty_rows, empty_cols, n_dup))
+	}
+
+	sum_dist := 0
+	for ix_p1 := 0; ix_p1 < len(galaxy_coords)-1; ix_p1++ {
+		for ix_p2 := ix_p1 + 1; ix_p2 < len(galaxy_coords); ix_p2++ {
+			// sum_dist += mahattan_dist(galaxy_coords[ix_p1], galaxy_coords[ix_p2])
+			// fmt.Printf("%d %d - sum_dist = %d\n", ix_p1, ix_p2, sum_dist)
+			sum_dist += mahattan_dist(galaxy_coords_shift[ix_p1], galaxy_coords_shift[ix_p2])
+		}
+	}
+
+	fmt.Printf("Part 1: Sum of distances: %d (correct: 9565386)\n", sum_dist)
+
+	// Work with sparse representation here. Otherwise we run out of memory
+	n_dup = 1_000_000
+	galaxy_coords_shift = []coords_t{}
+
+	for _, g := range galaxy_coords {
+		galaxy_coords_shift = append(galaxy_coords_shift, shift_position(g, empty_rows, empty_cols, n_dup))
+	}
+
+	sum_dist = 0
+	for ix_p1 := 0; ix_p1 < len(galaxy_coords_shift)-1; ix_p1++ {
+		for ix_p2 := ix_p1 + 1; ix_p2 < len(galaxy_coords_shift); ix_p2++ {
+			sum_dist += mahattan_dist(galaxy_coords_shift[ix_p1], galaxy_coords_shift[ix_p2])
+			// fmt.Printf("%d %d - sum_dist = %d\n", ix_p1, ix_p2, sum_dist)
+		}
+	}
+	// Guess: 874799690595 - wrong (too high)
+	fmt.Printf("Part 2: Sum of distances: %d (correct: )\n", sum_dist)
+
 }
--- 11/galaxy_test.go
+++ 11/galaxy_test.go
@@ -52,9 +52,10 @@ func Test_expand_array(t *testing.T) {
 	empty_cols := find_empty_cols(chart, nrow, ncol)
 	new_nrow := nrow + len(empty_rows)
 	new_ncol := ncol + len(empty_cols)
+	n_dup := 1
 
 	// Expand rows
-	chart_exp_rows := duplicate_row(chart, nrow, ncol, empty_rows)
+	chart_exp_rows := duplicate_row(chart, nrow, ncol, empty_rows, n_dup)
 
 	// fmt.Printf("\n\n---- Expanded rows chart - size: %d\n", len(chart_exp_rows))
 	// for ix_row := 0; ix_row < new_nrow; ix_row++ {
@@ -78,7 +79,8 @@ func Test_expand_array(t *testing.T) {
 	// 	fmt.Printf("\n")
 	// }
 
-	chart_exp := duplicate_col(chart_exp_rows, new_nrow, ncol, empty_cols)
+	chart_exp := duplicate_col(chart_exp_rows, new_nrow, ncol, empty_cols, n_dup)
+
 	// fmt.Printf("\n\n---- Expanding rows and columns - size: %d\n\n", len(chart_exp))
 	// for ix_row := 0; ix_row < new_nrow; ix_row++ {
 	// 	fmt.Printf("%02d: ", ix_row)
@@ -101,24 +103,76 @@ func Test_expand_array(t *testing.T) {
 
 }
 
-func Test_galaxy_dist(t *testing.T) {
-	fmt.Printf("\n\n--------- Expanding arrays --------\n\n")
+// func Test_expand_array_2(t *testing.T) {
+// 	fmt.Printf("\n\n--------- Expanding arrays (arbitrary size)--------\n\n")
+// 	nrow, ncol := 10, 10
+// 	chart := load_map("input_11_test1", nrow, ncol)
+
+// 	empty_rows := find_empty_rows(chart, nrow, ncol)
+// 	empty_cols := find_empty_cols(chart, nrow, ncol)
+
+// 	fmt.Printf("\n\n --- Original chart ---- \n\n")
+// 	for ix_row := 0; ix_row < nrow; ix_row++ {
+// 		fmt.Printf("%02d: ", ix_row)
+// 		for ix_col := 0; ix_col < ncol; ix_col++ {
+// 			fmt.Printf("%c", chart[ix_row*ncol+ix_col])
+// 		}
+// 		fmt.Printf("\n")
+// 	}
+
+// 	n_dup := 1
+// 	fmt.Printf("\n\n---- Expanded rows chart - n_dup: %d\nExpanding rows: ", n_dup)
+// 	for _, v := range empty_rows {
+// 		fmt.Printf("%02d ", v)
+// 	}
+// 	fmt.Printf("\n")
+// 	new_nrow := nrow + n_dup*len(empty_rows)
+// 	chart_exp_rows := duplicate_row(chart, nrow, ncol, empty_rows, n_dup)
+
+// 	for ix_row := 0; ix_row < new_nrow; ix_row++ {
+// 		fmt.Printf("%02d: ", ix_row)
+// 		for ix_col := 0; ix_col < ncol; ix_col++ {
+// 			fmt.Printf("%c", chart_exp_rows[ix_row*ncol+ix_col])
+// 		}
+// 		fmt.Printf("\n")
+// 	}
+
+// 	fmt.Printf("\n\n---- Expanded cols chart - n_dup: %d\nExpanding columns: ", n_dup)
+// 	for _, v := range empty_cols {
+// 		fmt.Printf("%02d ", v)
+// 	}
+// 	fmt.Printf("\n")
+// 	new_ncol := ncol + n_dup*len(empty_cols)
+// 	chart_exp_cols := duplicate_col(chart, nrow, ncol, empty_cols, n_dup)
+// 	for ix_row := 0; ix_row < nrow; ix_row++ {
+// 		fmt.Printf("%02d: ", ix_row)
+// 		for ix_col := 0; ix_col < new_ncol; ix_col++ {
+// 			fmt.Printf("%c", chart_exp_cols[ix_row*new_ncol+ix_col])
+// 		}
+// 		fmt.Printf("\n")
+// 	}
+
+// }
+
+func Test_part1(t *testing.T) {
+	fmt.Printf("\n\n--------- Testing part 1--------\n\n")
 	nrow, ncol := 10, 10
 	chart := load_map("input_11_test1", nrow, ncol)
 	empty_rows := find_empty_rows(chart, nrow, ncol)
 	empty_cols := find_empty_cols(chart, nrow, ncol)
 	new_nrow := nrow + len(empty_rows)
 	new_ncol := ncol + len(empty_cols)
+	n_dup := 1
 
-	chart_exp_rows := duplicate_row(chart, nrow, ncol, empty_rows)
-	chart_exp := duplicate_col(chart_exp_rows, new_nrow, ncol, empty_cols)
+	chart_exp_rows := duplicate_row(chart, nrow, ncol, empty_rows, n_dup)
+	chart_exp := duplicate_col(chart_exp_rows, new_nrow, ncol, empty_cols, n_dup)
 
 	galaxy_coords := galaxy_pos(chart_exp, new_nrow, new_ncol)
 
-	fmt.Printf("Found %d galaxies:\n", len(galaxy_coords))
-	for _, g := range galaxy_coords {
-		fmt.Printf("%d: [%02d,%02d]\n", g.id, g.row, g.col)
-	}
+	// fmt.Printf("Found %d galaxies:\n", len(galaxy_coords))
+	// for _, g := range galaxy_coords {
+	// 	fmt.Printf("%d: [%02d,%02d]\n", g.id, g.row, g.col)
+	// }
 
 	ix1, ix2, true_d := 4, 8, 9
 	// fmt.Printf("Distance %d [%02d,%02d] - %d [%02d,%02d] %d\n", galaxy_coords[ix1].id, galaxy_coords[ix1].row, galaxy_coords[ix1].col, galaxy_coords[ix2].id, galaxy_coords[ix2].row, galaxy_coords[ix2].col, mahattan_dist(galaxy_coords[ix1], galaxy_coords[ix2]))
@@ -143,4 +197,134 @@ func Test_galaxy_dist(t *testing.T) {
 		t.Fatalf("Mismatch: Distance(%d [%02d,%02d], %d [%02d,%02d]) = %d but should be %d\n", galaxy_coords[ix1].id, galaxy_coords[ix1].row, galaxy_coords[ix1].col, galaxy_coords[ix2].id, galaxy_coords[ix2].row, galaxy_coords[ix2].col, mahattan_dist(galaxy_coords[ix1], galaxy_coords[ix2]), true_d)
 	}
 
+	sum_dist := 0
+	for ix_p1 := 0; ix_p1 < len(galaxy_coords)-1; ix_p1++ {
+		for ix_p2 := ix_p1 + 1; ix_p2 < len(galaxy_coords); ix_p2++ {
+			sum_dist += mahattan_dist(galaxy_coords[ix_p1], galaxy_coords[ix_p2])
+		}
+	}
+
+	if sum_dist != 374 {
+		t.Fatalf("Sum of pair distances = %d, but should be 374\n", sum_dist)
+	}
+	fmt.Printf("Part 1: Sum of pair distances = %d\n", sum_dist)
+
+	// Approach 2: just find out by how much we have to shift each galaxy position
+	// when accounting for empty rows/cols
+	galaxy_coords_orig := galaxy_pos(chart, nrow, ncol)
+	galaxy_coords_shift := []coords_t{}
+	// fmt.Printf("Found %d galaxies:\n", len(galaxy_coords_orig))
+	for _, g := range galaxy_coords_orig {
+		// fmt.Printf("Original: %d - [%02d,%02d]\n", g.id, g.row, g.col)
+		galaxy_coords_shift = append(galaxy_coords_shift, shift_position(g, empty_rows, empty_cols, 1))
+		// fmt.Printf("Shifted: %d - [%02d,%02d]\n", galaxy_coords_shift[ix_g].id, galaxy_coords_shift[ix_g].row, galaxy_coords_shift[ix_g].col)
+	}
+
+	for ix_g, g := range galaxy_coords_shift {
+		// fmt.Printf("From dense:  id=%d, row=%d, col=%d\n", galaxy_coords[ix_g].id, galaxy_coords[ix_g].row, galaxy_coords[ix_g].col)
+		// fmt.Printf("From sparse: id=%d, row=%d, col=%d\n", g.id, g.row, g.col)
+		if (g.id != galaxy_coords[ix_g].id) || (g.col != galaxy_coords[ix_g].col) || (g.row != galaxy_coords[ix_g].row) {
+			fmt.Printf("Mismatch\n")
+		}
+	}
+
+	sum_dist = 0
+	for ix_p1 := 0; ix_p1 < len(galaxy_coords)-1; ix_p1++ {
+		for ix_p2 := ix_p1 + 1; ix_p2 < len(galaxy_coords); ix_p2++ {
+			sum_dist += mahattan_dist(galaxy_coords_shift[ix_p1], galaxy_coords_shift[ix_p2])
+		}
+	}
+	if sum_dist != 374 {
+		t.Fatalf("Sum of pair distances = %d, but should be 374\n", sum_dist)
+	}
+	fmt.Printf("Part 1: Sum of pair distances = %d\n", sum_dist)
+
 }
+
+func Test_part2(t *testing.T) {
+	fmt.Printf("\n\n --------- Testing Part 2 --------\n\n")
+
+	nrow, ncol := 10, 10
+	chart := load_map("input_11_test1", nrow, ncol)
+	empty_rows := find_empty_rows(chart, nrow, ncol)
+	empty_cols := find_empty_cols(chart, nrow, ncol)
+
+	n_dup_list := []int{10 - 1, 100 - 1}
+	answer_list := []int{1030, 8410}
+
+	galaxy_coords := galaxy_pos(chart, nrow, ncol)
+	fmt.Printf("Found %d galaxies:\n", len(galaxy_coords))
+
+	for ix_t, _ := range n_dup_list {
+		n_dup := n_dup_list[ix_t]
+
+		galaxy_coords_shift := []coords_t{}
+
+		for _, g := range galaxy_coords {
+			galaxy_coords_shift = append(galaxy_coords_shift, shift_position(g, empty_rows, empty_cols, n_dup))
+		}
+
+		sum_dist := 0
+		for ix_p1 := 0; ix_p1 < len(galaxy_coords_shift)-1; ix_p1++ {
+			for ix_p2 := ix_p1 + 1; ix_p2 < len(galaxy_coords_shift); ix_p2++ {
+				sum_dist += mahattan_dist(galaxy_coords_shift[ix_p1], galaxy_coords_shift[ix_p2])
+			}
+		}
+		if sum_dist != answer_list[ix_t] {
+			t.Fatalf("Sum of pair distances = %d, but should be %d\n", sum_dist, answer_list[ix_t])
+		}
+		fmt.Printf("n_dup = %d - Sum of pair distances = %d, should be %d\n", n_dup, sum_dist, answer_list[ix_t])
+	}
+
+}
+
+// func Test_part2(t *testing.T) {
+// 	fmt.Printf("\n\n--------- Testing Part 2 --------\n\n")
+// 	nrow, ncol := 10, 10
+// 	chart := load_map("input_11_test1", nrow, ncol)
+// 	empty_rows := find_empty_rows(chart, nrow, ncol)
+// 	empty_cols := find_empty_cols(chart, nrow, ncol)
+// 	n_dup := 9
+// 	new_nrow := nrow + n_dup*len(empty_rows)
+// 	new_ncol := ncol + n_dup*len(empty_cols)
+
+// 	chart_exp_rows := duplicate_row(chart, nrow, ncol, empty_rows, n_dup)
+// 	chart_exp := duplicate_col(chart_exp_rows, new_nrow, ncol, empty_cols, n_dup)
+
+// 	galaxy_coords := galaxy_pos(chart_exp, new_nrow, new_ncol)
+
+// 	fmt.Printf("Found %d galaxies:\n", len(galaxy_coords))
+// 	for _, g := range galaxy_coords {
+// 		fmt.Printf("%d: [%02d,%02d]\n", g.id, g.row, g.col)
+// 	}
+
+// 	sum_dist := 0
+// 	for ix_p1 := 0; ix_p1 < len(galaxy_coords)-1; ix_p1++ {
+// 		for ix_p2 := ix_p1 + 1; ix_p2 < len(galaxy_coords); ix_p2++ {
+// 			sum_dist += mahattan_dist(galaxy_coords[ix_p1], galaxy_coords[ix_p2])
+// 		}
+// 	}
+// 	if sum_dist != 1030 {
+// 		t.Fatalf("Sum of pair distances = %d, but should be 1030\n", sum_dist)
+// 	}
+
+// 	n_dup = 99
+// 	new_nrow = nrow + n_dup*len(empty_rows)
+// 	new_ncol = ncol + n_dup*len(empty_cols)
+
+// 	chart_exp_rows = duplicate_row(chart, nrow, ncol, empty_rows, n_dup)
+// 	chart_exp = duplicate_col(chart_exp_rows, new_nrow, ncol, empty_cols, n_dup)
+
+// 	galaxy_coords = galaxy_pos(chart_exp, new_nrow, new_ncol)
+
+// 	sum_dist = 0
+// 	for ix_p1 := 0; ix_p1 < len(galaxy_coords)-1; ix_p1++ {
+// 		for ix_p2 := ix_p1 + 1; ix_p2 < len(galaxy_coords); ix_p2++ {
+// 			sum_dist += mahattan_dist(galaxy_coords[ix_p1], galaxy_coords[ix_p2])
+// 		}
+// 	}
+// 	if sum_dist != 8410 {
+// 		t.Fatalf("Sum of pair distances = %d, but should be 8410\n", sum_dist)
+// 	}
+
+// }
